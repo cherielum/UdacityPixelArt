7cacef769891c9d2db2a5eb808187f3aa045a9c3
Cherie Lum <charismatic.cherie@gmail.com>
2018-03-09 12:46:50 -0500

    fixed table

--- designs.js
+++ designs.js
@@ -3,25 +3,28 @@ const color = $('#colorPicker');
 const table = $('#pixelCanvas');
 // Selecting size input
 const height = $('#inputHeight');
-const width = $('#inputWIdth');
+const width = $('#inputWeight');
 
 // When size is submitted by the user, calling makeGrid()
 
 $('#buttonPaint').on('click', function makeGrid(event) {
     event.preventDefault(); 
-    // console.log('button works!');
+    console.log('button works!');
     table.children().remove(); //clears grid
     let row = height.val(); 
-    let col = width.val(); 
+    let cols = width.val(); 
+    console.log(row, cols);
 
     //appending rows horizontally
     for(let x = 0; x <row; x++){
         table.append('<tr></tr>');
+       
     }
 
     //appending rows vertically
-    for(let y = 0; y<col; y++){
-        $('table tr').append('<td></td>'); 
+    for(let y = 0; y<cols; y++){
+        $('table tr').append('<td class = "box"></td>'); 
+        
     }
 });
 
