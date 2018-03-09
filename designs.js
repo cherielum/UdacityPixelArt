17d23dba03ccc4b3ab15570c8c0fa6aa6aca9a8a
Cherie Lum <charismatic.cherie@gmail.com>
2018-03-09 12:32:37 -0500

    adding button/table setup

--- designs.js
+++ designs.js
@@ -1,10 +1,28 @@
-// Select color input
-// Select size input
+// Selecting color input
+const color = $('#colorPicker');
+const table = $('#pixelCanvas');
+// Selecting size input
+const height = $('#inputHeight');
+const width = $('#inputWIdth');
 
-// When size is submitted by the user, call makeGrid()
+// When size is submitted by the user, calling makeGrid()
 
-function makeGrid() {
+$('#buttonPaint').on('click', function makeGrid(event) {
+    event.preventDefault(); 
+    // console.log('button works!');
+    table.children().remove(); //clears grid
+    let row = height.val(); 
+    let col = width.val(); 
 
-// Your code goes here!
+    //appending rows horizontally
+    for(let x = 0; x <row; x++){
+        table.append('<tr></tr>');
+    }
 
-}
+    //appending rows vertically
+    for(let y = 0; y<col; y++){
+        $('table tr').append('<td></td>'); 
+    }
+});
+
+//create table
