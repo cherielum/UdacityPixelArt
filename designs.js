164339b5f17aa6d60d54d90f6fe35973b8523120
Cherie Lum <charismatic.cherie@gmail.com>
2018-03-11 17:10:09 -0400

    adding functions

--- designs.js
+++ designs.js
@@ -9,12 +9,13 @@ const width = $('#inputWeight');
 
 $('#buttonPaint').on('click', function makeGrid(event) {
     event.preventDefault(); 
-    console.log('button works!');
     table.children().remove(); //clears grid
+    console.log('button works!');
     let row = height.val(); 
     let cols = width.val(); 
     console.log(row, cols);
 
+  
     //appending rows horizontally
     for(let x = 0; x <row; x++){
         table.append('<tr></tr>');
@@ -26,6 +27,13 @@ $('#buttonPaint').on('click', function makeGrid(event) {
         $('table tr').append('<td class = "box"></td>'); 
         
     }
+
+    //clear
+    $('#buttonClear').on('click', function makeGrid(event) {
+        event.preventDefault(); 
+        table.children().remove(); //clears grid
+    });
+
 });
 
 //create paint on canvas 
@@ -47,62 +55,54 @@ table.on('click', '.box', function(){
     });
   
   //making width/height form hide 
-  $(document).ready(function(){
-    $("#buttonPaint").click(function(){
-        $("#sizePicker").hide();
-    });
-    $("#buttonPaint").click(function(){
-        $("#sizePicker").show();
-    });
-});
-  
-//choose ideas 
-var NumberOfWords = 28
+//   $(document).ready(function(){
+//     $("#buttonPaint").click(function(){
+//         console.log('testing');
+//         $('form').hide();
+//     });
+//     $("#buttonPaint").click(function(){
+//         $("#sizePicker").show();
+//     });
+// });
 
-var words = new BuildArray(NumberOfWords)
-
-// Use the following variables to 
-// define your random words:
-words[1] = "escapology"
-words[2] = "brightwork"
-words[3] = "verkrampte"
-words[4] = "protectrix"
-words[5] = "nudibranch"
-words[6] = "grandchild"
-words[7] = "newfangled"
-words[8] = "flugelhorn"
-words[9] = "mythologer"
-words[10] = "pluperfect"
-words[11] = "jellygraph"
-words[12] = "quickthorn"
-words[13] = "rottweiler"
-words[14] = "technician"
-words[15] = "cowpuncher"
-words[16] = "middlebrow"
-words[17] = "jackhammer"
-words[18] = "triphthong"
-words[19] = "wunderkind"
-words[20] = "dazzlement"
-words[21] = "jabberwock"
-words[22] = "witchcraft"
-words[23] = "pawnbroker"
-words[24] = "thumbprint"
-words[25] = "motorcycle"
-words[26] = "cryptogram"
-words[27] = "torchlight"
-words[28] = "bankruptcy"
-
-function BuildArray(size){
-this.length = size
-for (var i = 1; i <= size; i++){
-this[i] = null}
-return this
+function hideFunction(){
+    var x = document.getElementById("sizePicker");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
 }
 
-function PickRandomWord(frm) {
-// Generate a random number between 1 and NumberOfWords
-var rnd = Math.ceil(Math.random() * NumberOfWords)
 
-// Display the word inside the text box
-frm.WordBox.value = words[rnd]
-}
+$("#buttonClear").on('click', function(){
+    $("#sizePicker").show();
+    });
+
+
+
+var ideas = ['Rock', 
+             'Fish', 
+             'Paper',
+             'Cow',
+             'Octopus',
+             'Dog',
+             'Pig',
+             'Backpack',
+             'Money', 
+             'iPad',
+             'Computer',
+             'Feet',
+             'T-shirt',
+             'Pants',
+             'Math problem',
+             'Smiley face'
+        
+        ];
+
+function pickRandomWord() {
+    // e.preventDefault(); 
+    var ideaToMake = Math.floor(Math.random()*(ideas.length));
+    document.getElementById('idea').innerHTML = ideas[ideaToMake]; 
+
+    }
